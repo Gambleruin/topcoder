c076eda65aa5666f112dfcc92f18e5b8160c7701
Gambleruin <dannylovewargame@gmail.com>
2018-05-11 20:37:16 -0300

    *

--- sp_topcoder.py
+++ sp_topcoder.py
@@ -1,23 +1,26 @@
 # topcoder
 import numpy as np
 class greedy_approach:
-    def get(self,n,want):
-        d = n+n-1-want
+    def get(self,n,s):
+        d = n+n-1-s
 
         if(d%2 ==0):
             return [n-1-d/2 ,n-d/2]
         elif(d%2 !=0):
-            return [want -n, n]
+            return [s -n, n]
  
-    def recur_max_pair_up(self,n,c,k,wantsum=None):
+    def recur_max_pair_up(self,n,c,k,s=None):
         if (n ==1):
             return []
-        if wantsum is None: 
-            wantsum = n+n-1
+        if s is None: 
+            s = n+n-1
         else:
-            g = self.get(n,wantsum)
-        g = self.get(n,wantsum)
+            g = self.get(n,s)
+        g = self.get(n,s)
         return g if k == 1 else self.recur_max_pair_up(g[0]-1,c,k-1,sum(g)-c)+g
+
+class dp_approach:
+
 
 if __name__ == '__main__':
     sp =greedy_approach()
