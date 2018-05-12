0772032932bb2e6714fcdeea675e40e137d369ae
Gambleruin <dannylovewargame@gmail.com>
2018-05-12 12:11:20 -0300

    *

--- sp_topcoder.py
+++ sp_topcoder.py
@@ -25,18 +25,18 @@ class dp_approach:
         self.n =n
         self.c =c
         self.k =k 
-        self.dp = np.zeros((k+1,n+1,2*n), dtype=bool)
-        self.prev =np.zeros((k+1, n+1, 2*n))
+        self.dp = np.zeros((k+1,n+1), dtype=bool)
+        self.prev =np.zeros((k+1, n+1))
 
-    def init(c, inx, y, is_):
+    def init(c, inx, j, is_):
         ans =np.zeros((inx*2));
         while(inx >= 1):
-            x =is_+(inx -1)*c -y
+            x =is_+(inx -1)*c -j
             ans[inx*2 -2] =x
-            ans[inx*2 -1] =y
-            int w =self.prev[inx][y][is_]
+            ans[inx*2 -1] =j
+            int w =self.prev[inx][j][is_]
             inx =inx -1;
-            y =w
+            j =w
         return ans
     
     def dy_pair_up(is_):
@@ -47,28 +47,29 @@ class dp_approach:
         for i in range(k+1):
             for j in range(self.n):
                 self.dp[i][j] =np.zeros((2*self.n+1), dtype =bool)
+                self.prev[i][j] =np.zeros((2*self.n+1))
                 for is_ in range(2*n):
-                    self.dp[i][y][is_] =False
+                    self.dp[i][j][is_] =False
         
         for i in range(k):
             for j =2 in range(self.n):
-                y =y-1
-                x =is_+(i-1)*self.c-y;
-                if(x >=y):
+                
+                x =is_+(i-1)*self.c-j;
+                if(x >=j):
                     break
                 if(x <=1):
                     continue;
                 if(i ==1):
-                    self.dp[i][y][is_] =True
+                    self.dp[i][j][is_] =True
                 else:
                     for w in range(x):
                         if(self.dp[i-1][w][is_]):
-                            self.dp[i][y][is_] =True
-                            self.prev[i][y][is_] =w
+                            self.dp[i][j][is_] =True
+                            self.prev[i][j][is_] =w
                             break
 
-                if(i ==k and self.dp[i][y][is_]):
-                    return init(self.c, i, y, is_)
+                if(i ==k and self.dp[i][j][is_]):
+                    return init(self.c, i, j, is_)
 
 if __name__ == '__main__':
     sp =greedy_approach()
