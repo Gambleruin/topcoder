5b5c819cf8456aa9da7f8b905ff85ba780ea3c3e
Gambleruin <dannylovewargame@gmail.com>
2018-05-12 14:02:45 -0300

    *

--- sp_topcoder.py
+++ sp_topcoder.py
@@ -25,54 +25,58 @@ class dp_approach:
         self.n =n
         self.c =c
         self.k =k 
-        self.dp = np.zeros((k+1,n+1), dtype=bool)
+        self.dp_table = np.zeros((k+1,n+1), dtype=bool)
         self.prev =np.zeros((k+1, n+1))
 
-    def init(c, inx, j, is_):
+    def trace(c, inx, j, is_):
         ans =np.zeros((inx*2));
         while(inx >= 1):
-            x =is_+(inx -1)*c -j
-            ans[inx*2 -2] =x
+            r =is_+(inx -1)*c -j
+            ans[inx*2 -2] =r
             ans[inx*2 -1] =j
-            int w =self.prev[inx][j][is_]
+            w =self.prev[inx][j][is_]
             inx =inx -1;
             j =w
         return ans
     
-    def dy_pair_up(is_):
+    def dy_pair_up():
+        inx =0
         ans =np.zeros((n*2));
         if(self.k >=self.n/2):
             return ans
 
         for i in range(k+1):
             for j in range(self.n):
-                self.dp[i][j] =np.zeros((2*self.n+1), dtype =bool)
+                self.dp_table[i][j] =np.zeros((2*self.n+1), dtype =bool)
                 self.prev[i][j] =np.zeros((2*self.n+1))
                 for is_ in range(2*n):
                     self.dp[i][j][is_] =False
         
         for i in range(k):
-            for j =2 in range(self.n):
-                
-                x =is_+(i-1)*self.c-j;
-                if(x >=j):
-                    break
-                if(x <=1):
-                    continue;
-                if(i ==1):
-                    self.dp[i][j][is_] =True
-                else:
-                    for w in range(x):
-                        if(self.dp[i-1][w][is_]):
-                            self.dp[i][j][is_] =True
-                            self.prev[i][j][is_] =w
+            j =2
+            for j in range(self.n):
+                for is_ in range(2*n):
+                    x =is_+(i-1)*self.c-j;
+                    if(x >=j):
+                        break
+                    if(x <=1):
+                        continue;
+                    if(i ==1):
+                        self.dp_table[i][j][is_] =True
+                    else:
+                        for w in range(x):
+                            if(self.dp_table[i-1][w][is_]):
+                                self.dp_table[i][j][is_] =True
+                                self.prev[i][j][is_] =w
+                                inx =is_
                             break
 
-                if(i ==k and self.dp[i][j][is_]):
-                    return init(self.c, i, j, is_)
+                if(i ==k and self.dp_table[i][j][inx]):
+                    return trace(self.c, i, j, inx)
 
 if __name__ == '__main__':
     sp =greedy_approach()
+
     result =sp.recur_max_pair_up(12, 7, 3, None)
     print(result)
 
