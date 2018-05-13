741babb61f93ed7f60aba994e67f8b8906403ab3
Gambleruin <dannylovewargame@gmail.com>
2018-05-13 09:52:33 -0300

    *

--- sp_topcoder.py
+++ sp_topcoder.py
@@ -25,60 +25,36 @@ class dp_approach:
         self.n =n
         self.c =c
         self.k =k 
-        self.dp_table = np.zeros((k+1, n+1, 2*self.n+1), dtype=bool)
-        self.prev =np.zeros((k+1, n+1, 2*self.n+1))
-
-    def trace(c, inx, j, is_):
-        ans =np.zeros((inx*2));
-        while(inx >= 1):
-            r =is_+(inx -1)*c -j
-            ans[inx*2 -2] =r
-            ans[inx*2 -1] =j
-            w =self.prev[inx][j][is_]
-            inx =inx -1;
-            j =w
-        print(ans)
-        return ans
-    
+        self.dp_table = np.zeros((k, n, 2*self.n), dtype=bool)
+        self.prev =np.zeros((k, n, 2*self.n))
+           
     def dy_pair_up(self):
-        inx =0
-        #ans =np.zeros((self.n*2));
-        #if(self.k >=self.n/2):
-        #    print('I was here')
-        #    return ans
-        for i in range(self.k+1):
+        for i in range(self.k):
             for j in range(self.n):
-                for is_ in range(2*self.n):
-                    self.dp_table[i][j][is_] =False
+                for s in range(2*self.n):
+                    self.dp_table[i][j][s] =False
         # solve
         for i in range(self.k):
-
-            j =2
             for j in range(self.n):
-                for is_ in range(2*self.n):
-                    x =is_+(i-1)*self.c-j;
-                    if(x >=j):
-                        print('I was here',x,j, is_)
+                for s in range(2*self.n):
+                    prev_s =self.sum_table[i][j-1][s]
+                    if (s >=val+c):
+                        x =s/2
+                        y =s-x
+                        while(x ==y):
+                            x =x-1
+                            y =y+1
+                    else
                         break
-                    if(x <=1):
-                        continue;
-                    if(i ==1):
-                        self.dp_table[i][j][is_] =True
-                    else:
-                        for w in range(x):
-                            if(self.dp_table[i-1][w][is_]):
-                                self.dp_table[i][j][is_] =True
-                                self.prev[i][j][is_] =w
-                                inx =is_
-                                print('I waswas here')
-                            break
-
-                if(i ==self.k and self.dp_table[i][j][inx]):
-                    return trace(self.c, i, j, inx)
+                    val =y
+                    self.dp_table[i][s] =True
+                    self.sum_table[x][y][val] =s
+    
+    return (self.dp_table, self.sum_table)
 
 if __name__ == '__main__':
     sp =greedy_approach()
-    dp =dp_approach(4,4,2)
+    dp =dp_approach(5,4,2)
     dp.dy_pair_up()
 
     # result =sp.recur_max_pair_up(12, 7, 3, None)
