b92cc8106f779756418bf50c4a282ffda9a601cb
Gambleruin <dannylovewargame@gmail.com>
2018-05-13 13:57:28 -0300

    *

--- sp_topcoder.py
+++ sp_topcoder.py
@@ -26,31 +26,36 @@ class dp_approach:
         self.c =c
         self.k =k 
         self.dp_table = np.zeros((k, n, 2*self.n), dtype=bool)
-        self.prev =np.zeros((k, n, 2*self.n))
-           
-    def dy_pair_up(self):
-        for i in range(self.k):
-            for j in range(self.n):
-                for s in range(2*self.n):
-                    self.dp_table[i][j][s] =False
-        # solve
-        for i in range(self.k):
-            for j in range(self.n):
-                for s in range(2*self.n):
-                    prev_s =self.sum_table[i][j-1][s]
-                    if (s >=val+c):
-                        x =s/2
-                        y =s-x
-                        while(x ==y):
-                            x =x-1
-                            y =y+1
-                    else
-                        break
-                    val =y
-                    self.dp_table[i][s] =True
-                    self.sum_table[x][y][val] =s
-    
-    return (self.dp_table, self.sum_table)
+        self.sum_table =np.zeros((k, n, 2*self.n))
+        p_sum =3
+        t_sum =0
+        while(p_sum+1 <=2*self.n) : 
+            p_sum =p_sum+1
+            for i in range(self.k): 
+                for s in range(p_sum, 2*self.n):     
+                    x =p_sum/2
+                    y =p_sum -x
+                    while(x ==y):
+                        x =x-1
+                        y =y+1
+                self.dp_table[i][s] =True
+                self.sum_table[x][y] =p_sum
+                p_sum =s+self.c
+
+            if(dp_table[i][s])
+                t_sum =t_sum +p_sum
+                max_sum =t_sum
+                continue
+            if(max_sum <t_sum):
+                self.dp_table[i][p_sum] =False
+
+
+
+        max_sum =t_sum
+
+        return (self.dp_table, self.sum_table)
+
+
 
 if __name__ == '__main__':
     sp =greedy_approach()
