ebc7387a7292362898daa8ebf398de965989e939
Gambleruin <dannylovewargame@gmail.com>
2018-05-13 14:58:05 -0300

    *

--- sp_topcoder.py
+++ sp_topcoder.py
@@ -27,10 +27,37 @@ class dp_approach:
         self.k =k 
         self.dp_table = np.zeros((k, n, 2*self.n), dtype=bool)
         self.sum_table =np.zeros((k, n, 2*self.n))
-        p_sum =3
         t_sum =0
-        while(p_sum+1 <=2*self.n) : 
-            p_sum =p_sum+1
+        min_sum =0
+        max_sum =0
+        for i in range(2*self.n):
+            max_sum =max_sum +min_sum
+            if(min_sum<3):
+                min_sum =2*i+1
+            for j in range(self.k): 
+                x =0
+                y =0
+                if (min_sum <=2*self.n):
+                    x =min_sum/2
+                    y =min_sum -x
+                    while(x ==y):
+                        x =x-1
+                        y =y+1
+                    dp_table[x][y] =True
+                    t_sum =t_sum+min_sum
+
+                if(dp_table[x][y]):
+                    sum_table[i][j] =min_sum
+                elif(t_sum >max_sum and j ==self.k):
+                    max_sum =t_sum
+                    break
+
+            min_sum =min_sum +self.c
+
+
+        while(init_s+1 <=2*self.n) : 
+            init_s =init_s+1
+            p_sum =init_s
             for i in range(self.k): 
                 for s in range(p_sum, 2*self.n):     
                     x =p_sum/2
@@ -41,13 +68,15 @@ class dp_approach:
                 self.dp_table[i][s] =True
                 self.sum_table[x][y] =p_sum
                 p_sum =s+self.c
+                
 
-            if(dp_table[i][s])
-                t_sum =t_sum +p_sum
-                max_sum =t_sum
-                continue
-            if(max_sum <t_sum):
-                self.dp_table[i][p_sum] =False
+
+
+        if(dp_table[i][s])
+            max_sum =t_sum
+            continue
+        if(max_sum <t_sum):
+            self.dp_table[i][p_sum] =False
 
 
 
