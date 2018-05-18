289d6049b7b634573a7496919f66524a0a78826e
Gambleruin <dannylovewargame@gmail.com>
2018-05-18 13:08:17 -0300

    *

--- HamiltonianPathsInGraph.py
+++ HamiltonianPathsInGraph.py
@@ -12,9 +12,10 @@ In the mathematical field of graph theory, a
 # sources from:
 # https://community.topcoder.com/stat?c=problem_solution&rd=17140&rm=&cr=40741634&pm=14881
 class HamiltonianPathsInGraph():
-	def __init__(self, MAXN):
+	def __init__(self, adj, MAXN):
 		self.dp =np.zeros((MAXN, 1<<MAXN))
-	# this is brute force
+		self.adj =adj
+	# brute force
 	def ham_bf(self, s):
 		if len(s) ==0:
 			return []
@@ -31,24 +32,42 @@ class HamiltonianPathsInGraph():
 	# coding resources
 	# https://www.hackerearth.com/practice/algorithms/graphs/hamiltonian-path/tutorial/
 	# https://www.hackerearth.com/practice/notes/bit-manipulation/
-	def check_i_th_bit_is_set(self, n, i):
-		if(n&(1<<i) ):
+	def check_i_th_bit_is_set(self, i, j):
+		if(i&(1<<j) ):
 			return True
 		else:
 			return False
 
+	def check_subset_path(self, i, j):
+		if(i^(1<<j)):
+			return True
+		else:
+			return False
 
+	# dynamic programming
 	def ham_dp(self, s, adj, n):
 		for i in range(n):
 			self.dp[i][1<<i] =True
 
-		for i in range()
+		for i in range(1<<n):
+			for j in range(n):
+				if(check_i_th_bit_is_set(i, j)):
+					for k in range(n):
+						if(check_i_th_bit_is_set(i, k) and self.adj[k][j] and k!=j and dp[k][check_subset_path(i, j)] ):
+							self.dp[j][i] =True
+							break
 
+	def solve(self, n):
+		for i in range(n):
+			if (self.dp[i][(1<<n) -1]):
+				return True
+		return False
 
-
-	def findPath(self, X):
+	def brute_force_findPath(self, X):
 		self.X =X
 		return self.ham_bf(set(range(len(X))))
+
+	def dynamic_programming_findPath(self, X):
 
 if __name__ == '__main__':
 	x =[['.','+'],
