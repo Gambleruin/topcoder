daf57a5669018226d82ec2c2103f2ec8d5d80535
Gambleruin <dannylovewargame@gmail.com>
2018-05-22 18:09:44 -0300

    *

--- TheRoundCityDiv1.cpp
+++ TheRoundCityDiv1.cpp
@@ -9,18 +9,35 @@ https://community.topcoder.com/stat?c=problem_solution&rd=17158&rm=331262&cr=105
 
 difficulty level: easy
 */
+
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include <algorithm>
+#include <numeric>
+#include <bitset>
+#include <array>
+#include <climits>
+
+using namespace std;
+
+#define M 32
+#define std::bitset<M> b
 
 class TheRoundCityDiv1 {
     int *divisors = new int[30];
+
+    int bit_cont( int x){
+    	return b.count(x);
+    }
  
     long find(int r) {
         long res = 0;
         if (r >= 1) ++res;
         if (r >= 2) ++res;
-        int[] d = new int[r + 1];
+        
+        int *d = new int[r + 1];
+        
         for (int i = 2; i <= r; ++i)
             if (d[i] == 0) {
                 for (int j = i; j <= r; j += i)
@@ -31,12 +48,13 @@ class TheRoundCityDiv1 {
         long y = r;
         for (long x = 2; x <= r; ++x) {
             while (x * x + y * y > r * (long) r) --y;
-            res += 2 * countUpTo((int) Math.min(x, y), (int) x, d);
+            res += 2 * countUpTo((int) min(x, y), (int) x, d);
         }
         return res * 4;
+            
     }
- 
-    int countUpTo(int max, int relativelyPrimeWith, int[] d) {
+ 	
+    int countUpTo(int max, int relativelyPrimeWith, int d[]) {
         int nd = 0;
         int tmp = relativelyPrimeWith;
         while (tmp > 1) {
@@ -51,12 +69,15 @@ class TheRoundCityDiv1 {
             int by = 1;
             for (int i = 0; i < nd; ++i) if ((mask & (1 << i)) != 0) by *= divisors[i];
             int got = max / by;
-            if (Integer.bitCount(mask) % 2 == 0) res += got;
+            if (bit_cont(mask) % 2 == 0) res += got;
             else res -= got;
         }
         return res;
     }
- 
+};
+
+int main(){
+	return 0;
 }
 
 
