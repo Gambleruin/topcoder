94d29bfa5bfefe92716450667d2d73e46718e718
Gambleruin <dannylovewargame@gmail.com>
2018-05-23 10:46:06 -0300

    *

--- TheRoundCityDiv1.cpp
+++ TheRoundCityDiv1.cpp
@@ -1,6 +1,6 @@
 /*
 SRM 734
-(this is only for programming practise as all implementations are adapted, I do not implement them originally )
+(this is only for programming practise )
 TheRoundCityDiv1
 The problem statement:
 https://community.topcoder.com/stat?c=problem_statement&pm=14897
@@ -14,10 +14,6 @@ difficulty level: easy
 #include <stdlib.h>
 #include <math.h>
 #include <algorithm>
-#include <numeric>
-#include <bitset>
-#include <array>
-#include <climits>
 
 using namespace std;
 
@@ -38,6 +34,7 @@ class TheRoundCityDiv1 {
         	if (r >= 2) ++res;
         
         	int *d = new int[r + 1];
+        	printf("%i\n\n", int(sizeof(d[0])));
         
         	for (int i = 2; i <= r; ++i)
             	if (d[i] == 0) {
@@ -46,29 +43,39 @@ class TheRoundCityDiv1 {
                         	d[j] = i;
                     	}
             	}
-        int y = r;
-        for (int x = 2; x <= r; ++x) {
-            while (x * x + y * y > r * (int) r) --y;
-            	res += 2 * countUpTo((int) min(x, y), (int) x, d);
-            	printf("%i\n\n", res);
-        }
-        printf("was I here?%i\n\n", res);
-        return res * 4;
+
+        	int y = r;
+
+        	for (int i =0; i<(sizeof(d)/sizeof(d[0]));i++)
+        	{
+        		printf("%ithis is absurd\n\n",d[i]);
+        	}
+
+        	for (int x = 2; x <= r; ++x) {
+            	while (x * x + y * y > r * (int) r) --y;
+            		//printf("%i%i\n\n\n\n\n\n", x, y);
+            		res += 2 * countUpTo((int) min(x, y), (int) x, d);
+            		//break;
+            		//printf("%i\n\n", res);
+        	}
+        	//printf("was I here?%i\n\n", res);
+        	return res * 4;
             
     	}
  	
     	int countUpTo(int max, int relativelyPrimeWith, int d[]) {
+    		//printf("%i\n%ithis is\n\n", max, relativelyPrimeWith);
         	int nd = 0;
         	int tmp = relativelyPrimeWith;
         	
-
         	while (tmp > 1) {
-        		//printf("was I here?\n\n\n\n");
+        		
             	int a = d[tmp];
             	divisors[nd++] = a;
             	while (tmp % a == 0) {
                 	tmp /= a;
             	}
+            	//printf("was I here%i%i%i?\n\n\n\n",a,divisors[nd],tmp);
         	}
         	int res = 0;
         	for (int mask = 0; mask < (1 << nd); ++mask) {
@@ -78,6 +85,7 @@ class TheRoundCityDiv1 {
             	if (bit_cont(mask) % 2 == 0) res += got;
             	else res -= got;
         	}
+
         	return res;
     	}
 };
