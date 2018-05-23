a6aa8554165fa6ee55cb1647a597a9336a1d4d47
Gambleruin <dannylovewargame@gmail.com>
2018-05-23 14:59:59 -0300

    *

--- TheRoundCityDiv1.cpp
+++ TheRoundCityDiv1.cpp
@@ -5,100 +5,99 @@ TheRoundCityDiv1
 The problem statement:
 https://community.topcoder.com/stat?c=problem_statement&pm=14897
 sources from:
-https://community.topcoder.com/stat?c=problem_solution&rd=17158&rm=331262&cr=10574855&pm=14897
-original code was in java, the modification of the code is tend to make it more concise and efficient 
+https://community.topcoder.com/stat?c=problem_solution&rd=17158&rm=&cr=22907549&pm=14897
 difficulty level: easy
 */
 
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
+typedef long long ll;
+const int MAXN =1000100;
 
 class TheRoundCityDiv1 {
 	public:
-    	int *divisors = new int[300];
-
+		// the function might not be needed
     	int bit_cont(unsigned int x){
     		int count;
     		for(count =0; x !=0; count++, x &=x-1);
     		//printf("%i\n", count);
     		return count;
     	}
- 
-    	int find(int r) {
-        	int res = 0;
-        	if (r >= 1) ++res;
-        	if (r >= 2) ++res;
-        
-        	int *d = new int[r + 1];
-        	printf("%i\n\n", int(sizeof(d[0])));
-        
-        	for (int i = 2; i <= r; ++i)
-            	if (d[i] == 0) {
-                	for (int j = i; j <= r; j += i)
-                    	if (d[j] == 0) {
-                        	d[j] = i;
-                    	}
-            	}
-
-        	int y = r;
-
-        	for (int i =0; i<(sizeof(d)/sizeof(d[0]));i++)
-        	{
-        		printf("%ithis is absurd\n\n",d[i]);
-        	}
-
-        	for (int x = 2; x <= r; ++x) {
-            	while (x * x + y * y > r * (int) r) --y;
-            		//printf("%i%i\n\n\n\n\n\n", x, y);
-            		res += 2 * countUpTo((int) min(x, y), (int) x, d);
-            		//break;
-            		//printf("%i\n\n", res);
-        	}
-        	//printf("was I here?%i\n\n", res);
-        	return res * 4;
-            
-    	}
- 	
-    	int countUpTo(int max, int relativelyPrimeWith, int d[]) {
-    		//printf("%i\n%ithis is\n\n", max, relativelyPrimeWith);
-        	int nd = 0;
-        	int tmp = relativelyPrimeWith;
-        	
-        	while (tmp > 1) {
-        		
-            	int a = d[tmp];
-            	divisors[nd++] = a;
-            	while (tmp % a == 0) {
-                	tmp /= a;
-            	}
-            	//printf("was I here%i%i%i?\n\n\n\n",a,divisors[nd],tmp);
-        	}
-        	int res = 0;
-        	for (int mask = 0; mask < (1 << nd); ++mask) {
-            	int by = 1;
-            	for (int i = 0; i < nd; ++i) if ((mask & (1 << i)) != 0) by *= divisors[i];
-            		int got = max / by;
-            	if (bit_cont(mask) % 2 == 0) res += got;
-            	else res -= got;
-        	}
-
-        	return res;
-    	}
+    	long long find(int); 
 };
 
-typedef int (TheRoundCityDiv1::*METHOD)(int);
+int np[MAXN];
 
+ll gres(int x, int y){
+	vector<int> v;
+	while (x>1){
+		int p =np[x];
+		v.push_back(p);
+		while(x%p ==0)
+			x /=p;
+	}
+
+	int ans =0;
+	int nv =v.size();
+	for (int i =0; i <(i << nv); i++)
+	{
+		int np =1, cv =1;
+		for(int j =0; j<nv; j++){
+			if(i & (1 <<j))
+			{
+				cv =-cv;
+				np =np*v[j];
+			}
+		
+		}
+		ans +=cv*(y/np);
+	}
+	return ans;
+}
+
+ll TheRoundCityDiv1::find(int r){
+	for (int i =0; i<MAXN; i++)
+		np[i] =-1;
+
+	for (int i =2; i<MAXN; i++)
+	{
+		if(np[i] ==-1)
+		{
+			for(int j =i; j<MAXN; j+=i)
+			{
+				if(np[j] ==-1)
+					np[j] =i;
+			}
+		}
+	}
+	ll ans =4;
+	ll ny =r;
+	for (int i =1; i <=r; i++)
+	{
+		ll nhi =r*(ll)r -i*(ll)i;
+		while(ny*ny>nhi)
+			ny--;
+		ans +=4*gres(i, ny);
+	}
+	return ans;
+}
+
+typedef ll (TheRoundCityDiv1::*METHOD)(int);
 int main(){
-
+	
 	TheRoundCityDiv1* obj =new TheRoundCityDiv1;
 	METHOD Action =&TheRoundCityDiv1::find;
-	int res =(obj->*Action)(3);
+	int res =(obj->*Action)(47);
 	printf("%i\n\n", res);
 	delete obj;
+	
+	
+
 	return 0;
 }
 
