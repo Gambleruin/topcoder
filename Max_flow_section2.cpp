a6f1269106ef1e4e95e847829c4a5d2ce9d64d9f
Gambleruin <dannylovewargame@gmail.com>
2018-05-27 22:05:52 -0300

    *

--- Max_flow_section2.cpp
+++ Max_flow_section2.cpp
@@ -56,7 +56,8 @@ class RookAttack
 
 public:	
 	int rows, cols;
-	
+
+	// use dfs
 	int find_path(int source)  
 	{  
 		/*
@@ -73,18 +74,23 @@ public:
             	}  
             	v[i] = true;  
             	if (find_path(col_match[i])) {  
-                	col_match[i] = source;  
-                	printf("but what about here?\n\n\n\n\n\n\n\n");    
+                	col_match[i] = source;     
                 	return 1;  
             	}  
     	}  
     return 0;  
 	} 
 
-	//use bfs to find path (gredily )
-	int find_find_gred(int source)
+	    int bfs()  
+    {         
+ 
+    } 
+
+	//use bfs to find path (gredily ) with Ford–Fulkerson algorithm
+	int find_path_gred(int source)
 	{
 		//the row-vertex that precedes x in the path
+		/*
 		vector<int> from;
 		memset(from, -1, sizeof(from));
 		from[source] =source;
@@ -95,12 +101,101 @@ public:
 		while (!found_path && !q.empty()){
 
 		}
+		*/
+
+		memset(v, 0, sizeof(v));  
+        memset(from, -1, sizeof(from));  
+        queue <int> Q;  
+        int start = rows, end = rows + cols + 1; 
+        v[start] = true;  
+        int where;  
+ 
+        bool found_path= false;  
+        while (!Q.empty()) {  
+            where = Q.front();  
+            Q.pop();  
+            for (int i = 0; i <= rows + cols + 1; i++) {  
+                if (cap[where][i] > 0 && !v[i]) {  
+                    from[i] = where;  
+                    v[i] = true;  
+                    if (i == end) {  
+                        found_path = true;  
+                        break;  
+                    }  
+                    Q.push(i);  
+                }  
+            }  
+            if (found_path) {  
+                break;  
+            }  
+        }  
+
+        int path_cap = INF;  
+        where = end;  
+        while (from[where] != -1) {  
+            int pre = from[where];  
+            path_cap = min(path_cap, cap[pre][where]);  
+            where = pre;  
+        }  
+
+        where = end;  
+        while (from[where] != -1) {  
+            int pre = from[where];  
+            cap[pre][where] -= path_cap;  
+            cap[where][pre] += path_cap;  
+            where = pre;  
+        }  
+          
+        return (path_cap == INF ? 0 : path_cap); 
 	}
-    // reduction to max_flow using Ford–Fulkerson algorithm with dfs
+	// almost identical, but you know I am lazy
 	int b_match(int rows, int cols, vector <string> cutouts){
 		int res = 0;  
         this->rows = rows;  
-        this->cols = cols;  
+        this->cols = cols; 
+
+        memset(cut, 0, sizeof(cut));  
+ 
+        string S;  
+        for (int i = 0; i < cutouts.size(); i++) {  
+            S += cutouts[i] + ", ";  
+        }             
+        for (int i = 0; i < S.size(); i++) {  
+            if (',' == S[i]) {  
+                S[i] = ' ';  
+            }  
+        }  
+        int r, c;  
+        stringstream ss(S);  
+       
+        while (ss >> r >> c) {  
+            cut[r][c + rows + 1] = true;  
+        }  
+  
+        memset(cap, 0, sizeof(cap));  
+        for (int i = 0; i < rows; i++) {  
+            for (int j = rows + 1; j < rows + cols + 1; j++) {  
+                if (!cut[i][j]) {  
+                    cap[i][j] = 1;       
+                }  
+                cap[j][rows + cols + 1] = 1;    
+            }  
+            cap[rows][i] = 1;   
+        }  
+        int add;  
+        while ( (add = bfs()) != 0 ) {  
+            res += add;  
+        }  
+        return res;  
+
+	}
+
+    // reduction to max_flow using  dfs
+	int d_match(int rows, int cols, vector <string> cutouts){
+		int res = 0;  
+        this->rows = rows;  
+        this->cols = cols; 
+
         memset(cut, 0, sizeof(cut));   
         string S;  
         for (int i = 0; i < cutouts.size(); i++) {  
@@ -132,7 +227,7 @@ int main(){
 	int col =8;
 	vector<string> cuts= {};
 	RookAttack* obj =new RookAttack;
-	METHOD Action =&RookAttack::b_match;
+	METHOD Action =&RookAttack::d_match;
 	int res =(obj->*Action)(row, col, cuts);
 	printf("\n%i\n\n", res);
 	delete obj;
