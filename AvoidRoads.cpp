e8aa72d68e11df6ba7e103eef73932aceb9d175a
Gambleruin <dannylovewargame@gmail.com>
2018-05-28 19:59:17 -0300

    *

---
+++ AvoidRoads.cpp
@@ -0,0 +1,12 @@
+/*
+
+TCO 2003 - Semifinal Round 4 Division 1 [topcoder]
+problem:
+AvoidRoads
+
+problem statement:
+https://community.topcoder.com/stat?c=problem_statement&pm=1889&rd=4709
+Difficult level: easy
+
+*/
+
