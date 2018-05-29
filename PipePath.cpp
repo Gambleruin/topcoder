ed368ffeef1a98df555db021663be518881232bf
Gambleruin <dannylovewargame@gmail.com>
2018-05-29 15:01:37 -0300

    *

---
+++ PipePath.cpp
@@ -0,0 +1,26 @@
+/*
+TCO 2003 - Semifinal Round 4 Division 1 [topcoder]
+problem:
+PipePath
+
+problem statement:
+https://community.topcoder.com/stat?c=problem_statement&pm=1889&rd=4709
+Difficult level: medium
+
+this problem can be solved using dynamic programming 
+*/
+
+/*
+double ratio = 0;
+	foreach(unique capacity cap){
+		find the shortest path from source to sink,
+		   each of whose edges has a capacity of at least cap
+		if(there is some path){
+			ratio = max(ratio, cap/pathLength)
+		}
+	}
+	return ratio;
+
+*/
+
+
